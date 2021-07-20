7a86a4cd6c8838b7fd47a2c39f7e704558d7bd7d
geek854 <geekbrains854@gmail.com>
2021-07-20 11:29:24 +0300

    домашняя работа 2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,62 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import Message from './Message.js';
+import { AUTHORS } from './constants'
 
-function Message(props){
-  return <p className="Message_border">Message - {props.text} </p>  
-}
 function App() {
-    return ( <div className = "App">
-        <header className = "App-header" >
-        <img src = { logo }className = "App-logo"alt = "logo" />
-        <p> Edit < code > src / App.js </code> and save to reload.</p >
-        <a className = "App-link" href = "https://reactjs.org" target = "_blank" rel = "noopener noreferrer" > Learn React </a>    
-        <Message text="Welcome!"/>
-        </header> 
+    const [messageList, setMessageList] = React.useState([])
+    const [inputValue, setInputValue] = React.useState('')
+
+
+    React.useEffect(() => {
+        if (
+            messageList.length&&
+            messageList[messageList.length-  1].author !== AUTHORS.BOT
+        ) {
+            setTimeout(
+                () =>
+                    setMessageList((currentMessageList) => [
+                        ...currentMessageList,
+                        { author: AUTHORS.BOT, text: 'Привет' },
+                    ]),
+                1500
+            )
+        }
+    }, [messageList])
+
+    const handleMessageChange = (e) => {
+        setInputValue(e.target.value)
+    }
+    const handleMessageSubmit = (e) => {
+        e.preventDefault()
+
+        setMessageList((currentMessageList) => [
+            ...currentMessageList,
+            { author: AUTHORS.ME, text: inputValue },
+        ])
+        setInputValue('')
+    }
+
+    return (
+        <div className="app">
+                {messageList.map((message, index) => (
+                    <Message 
+                        key={index}
+                        text={message.text}
+                        author={message.author}
+                    />
+                ))} 
+            <form className="app__form" onSubmit={handleMessageSubmit}>
+                <input
+                    required
+                    className="bordered"
+                    placeholder="Введите сообщение"
+                    value={inputValue}
+                    onChange={handleMessageChange}
+                />
+                <button>Отправить</button>
+            </form>
         </div>
-    );
+    )
 }
 export default App;
