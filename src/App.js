4ef29c8fc4b3b8cfc35a1e581d96d47707b430f4
geek854 <geekbrains854@gmail.com>
2021-07-27 19:45:54 +0300

    домашка 4

--- src/App.js
+++ src/App.js
@@ -1,109 +1,56 @@
 import React from 'react'
-import './App.css';
-import Message from './Message.js';
-import { AUTHORS } from './constants';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
+import './App.css'
+import Router from '../src/Router'
+import { Link } from 'react-router-dom'
 
 function App() {
-    const [messageList, setMessageList] = React.useState([])
-    const [inputValue, setInputValue] = React.useState('')
-
-
-    React.useEffect(() => {
-        if (
-            messageList.length&&
-            messageList[messageList.length-  1].author !== AUTHORS.BOT
-        ) {
-            setTimeout(
-                () =>
-                    setMessageList((currentMessageList) => [
-                        ...currentMessageList,
-                        { author: AUTHORS.BOT, text: 'Привет' },
-                    ]),
-                1500
-            )
-        }
-    }, [messageList])
-
-    const handleMessageChange = (e) => {
-        setInputValue(e.target.value)
-    }
-    const handleMessageSubmit = (e) => {
-        e.preventDefault()
-
-        setMessageList((currentMessageList) => [
-            ...currentMessageList,
-            { author: AUTHORS.ME, text: inputValue },
-        ])
-        setInputValue('')
-    }
     const [chats, setChats] = React.useState([
         { id: 'chat1', name: 'Чат 1' },
         { id: 'chat2', name: 'Чат 2' },
         { id: 'chat3', name: 'Чат 3' },
     ])
     const [currentChat, setCurrentChat] = React.useState(chats[0])
+
     const handleChangeChat = (chat) => setCurrentChat(chat)
+
+    const handleAddChat = (chatName) => {
+        setChats((currentChats) => [
+            ...currentChats,
+            { name: chatName, id: `chat${Date.now()}` },
+        ])
+    }
+
+    const handleRemoveChat = (chatId) => {
+        setChats((currentChats) =>
+            currentChats.filter((chat) => chat.id !== chatId)
+        )
+    }
+
+    const handleIsChatExists = React.useCallback(
+        (chatId) => {
+            return Boolean(chats.find((chat) => chat.id === chatId))
+        },
+        [chats]
+    )
+
     return (
         <div className="app">
-            <div>
-            <List>
-                {chats.map((chat) => {
-                    return (
-                    <ListItem
-                        button
-                        key={chat.id}
-                        selected={chat.id === currentChat.id}
-                        onClick={() => handleChangeChat(chat)}
-                    >
-                        {chat.name}
-                    </ListItem>)
-                })}
-            </List>
+            <div className="header bordered">
+                <Link to="/">Home</Link>
+                <Link to="/chats">Chats</Link>
+                <Link to="/profile">Profile</Link>
             </div>
-            
-            <div className="app__message">
-                {messageList.map((message, index) => (
-                <Message 
-                    key={index}
-                    text={message.text}
-                    author={message.author}
-                />
-                ))} 
-            <form className="app__form" onSubmit={handleMessageSubmit}>
-            <TextField 
-                fullWidth 
-                required
-                autoFocus
-                label="Сообщение"
-                placeholder="Введите сообщение"
-                // variant="outlined"
-                value={inputValue}
-                onChange={handleMessageChange}
-                />
-                {/* <button>Отправить</button> */}
-                <Button variant="contained" color="primary">
-                Primary
-                </Button>
-            </form>
-        </div>
-                
+
+            <Router
+                chats={chats}
+                currentChat={currentChat}
+                onCurrentChatChange={handleChangeChat}
+                getIsChatExists={handleIsChatExists}
+                onAddChat={handleAddChat}
+                onRemoveChat={handleRemoveChat}
+            />
         </div>
     )
 }
-export default App;
 
-
-
-
-
-
-
-
-
-
-
-
+export default App
---
+++ src/Chat.js
@@ -0,0 +1,79 @@
+import React from 'react'
+import { Redirect, useParams } from 'react-router'
+import Message from '../src/Message'
+import Input from '../src/Input'
+import usePrevious from '../../hooks/usePrevious'
+import { AUTHORS } from '../src/constants'
+
+const Chat = (props) => {
+    const { getIsChatExists } = props
+    const { chatId } = useParams()
+
+    const [messageList, setMessageList] = React.useState([])
+
+    const timer = React.useRef(null)
+    const prevMessageList = usePrevious(messageList)
+
+    React.useEffect(() => {
+        if (
+            prevMessageList?.length < messageList.length &&
+            messageList[messageList.length - 1].author !== AUTHORS.BOT
+        ) {
+            timer.current = setTimeout(
+                () =>
+                    setMessageList((currentMessageList) => [
+                        ...currentMessageList,
+                        { author: AUTHORS.BOT, text: 'Привет' },
+                    ]),
+                1500
+            )
+        }
+    }, [messageList, prevMessageList])
+
+    React.useEffect(() => {
+        return () => {
+            clearTimeout(timer.current)
+        }
+    }, [])
+
+    const handleMessageSubmit = (newMessageText) => {
+        setMessageList((currentMessageList) => [
+            ...currentMessageList,
+            { author: AUTHORS.ME, text: newMessageText },
+        ])
+    }
+
+    const isChatExists = React.useMemo(
+        () => getIsChatExists(chatId),
+        [getIsChatExists, chatId]
+    )
+
+    if (!isChatExists) {
+        return <Redirect to="/chats" />
+    }
+
+    return (
+        <div className="chat">
+            {messageList.length ? (
+                <div className="bordered">
+                    {messageList.map((message, index) => (
+                        <Message
+                            key={index}
+                            text={message.text}
+                            author={message.author}
+                            render={({ className }) => (
+                                <span className={className}>
+                                    {/*{Date.now().toLocaleString()}*/}
+                                </span>
+                            )}
+                        />
+                    ))}
+                </div>
+            ) : null}
+
+            <Input onSubmit={handleMessageSubmit} />
+        </div>
+    )
+}
+
+export default Chat
